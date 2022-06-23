8c44aeada376aa95e003053ca51f9428fae333d3
manjukalal <manjukalal1496@gmail.com>
2022-06-23 21:51:47 +0530

    Added:inserting numbers to binary tree

--- src/com/bridgelabz/BinarySearchTree.java
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -6,8 +6,20 @@ public class BinarySearchTree {
         BinaryTreeLogic tree = new BinaryTreeLogic();
         Node node = new Node();
         tree.insert(node, 56);
-        tree.insert(node,30);
-        tree.insert(node,70);
+        tree.insert(node, 30);
+        tree.insert(node, 70);
+        tree.insert(node, 22);
+        tree.insert(node, 40);
+        tree.insert(node, 11);
+        tree.insert(node, 3);
+        tree.insert(node, 16);
+        tree.insert(node, 60);
+        tree.insert(node, 95);
+        tree.insert(node, 65);
+        tree.insert(node, 63);
+        tree.insert(node, 67);
+        System.out.print("Level order traversal of binary tree is ");
+        tree.inorderTraversal(node);
 
     }
 }
--- src/com/bridgelabz/BinaryTreeLogic.java
+++ src/com/bridgelabz/BinaryTreeLogic.java
@@ -1,37 +1,47 @@
 package com.bridgelabz;
-
-    class Node {
-        int data;
-        Node left, right;
-    }
-
+class Node {
+    int data;
+    Node left, right;
+}
 public class BinaryTreeLogic {
-        public Node insert(Node node, int data) {
-            if (node == null) {
-                return createNode(data);
-
-            }
-            if (data < node.data) {
-                node.left = insert(node.left, data);
-
-            } else if (data > node.data) {
-                node.right = insert(node.right, data);
-                System.out.println(data);
-            }
-
-            return node;
+    public Node insert(Node node, int data) {
+        if (node == null) {
+            return createNode(data);
 
         }
+        if (data < node.data) {
+            node.left = insert(node.left, data);
 
-        public Node createNode(int data) {
-            Node root = new Node();
-            root.data = data;
-            root.left = null;
-            root.right = null;
-            return root;
+        } else if (data > node.data) {
+            node.right = insert(node.right, data);
+
         }
+        return node;
 
     }
+
+    public Node createNode(int data) {
+        Node root = new Node();
+        root.data = data;
+        root.left = null;
+        root.right = null;
+        return root;
+    }
+
+    public void inorderTraversal(Node node) {
+        if (node == null)
+            System.out.println("Tree is empty");
+        else {
+            if (node.left != null)
+                inorderTraversal(node.left);
+            System.out.print(node.data + " ");
+            if (node.right != null)
+                inorderTraversal(node.right);
+
+        }
+    }
+}
+
 
 
 
